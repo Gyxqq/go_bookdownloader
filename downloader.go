ded16b9ad9f24b07f023fca5249845a9d42d4d5e
gyxqqq <2277904687@qq.com>
2025-07-29 13:56:24 +0800

    feat(downloader): 实现小说下载器的基础功能
    - 添加 Downloader 接口及其实现结构 DownloaderImpl
    - 实现 Download 方法，包括获取小说名称和章节 URL 列表
    - 添加 Get_Book_Name 和 Get_Chapter_Urls 方法
    - 集成 logrus 日志库，增强日志记录功能
    - 优化错误处理和日志输出

--- downloader.go
+++ downloader.go
@@ -1,13 +1,18 @@
 package godown
 
 import (
-	"regexp"
+	"errors"
+	"github.com/sirupsen/logrus"
+	"io"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 type Downloader interface {
 	Download(url string) (Book, error)
 	Get_Book_Name(desc string) (string, error)
+	Get_Chapter_Urls(content string) ([]string, bool, error)
 }
 type Book struct {
 	name     string
@@ -19,12 +24,73 @@ type Chapter struct {
 	content string
 }
 type DownloaderImpl struct {
-	Client *http.Client
+	Client        *http.Client
 	name_regex    *regexp.Regexp
+	chapter_regex *regexp.Regexp
 	title_regex   *regexp.Regexp
 	content_regex *regexp.Regexp
 }
 
 func (d *DownloaderImpl) Download(url string) (Book, error) {
+	d.Client = &http.Client{}
+	book := Book{}
+	resp, err := d.Client.Get(url)
+	if err != nil {
+		logrus.WithField("downloader", "Download").Error("Error when get book main page")
+		return book, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logrus.WithField("downloader", "Download").Error("Error when io read book main page")
+		return book, err
+	}
+	book.name, err = d.Get_Book_Name(string(body))
+	if err != nil {
+		logrus.WithField("downloader", "Download").Error("Error when get book name")
+		return book, err
+	}
+	chapter_urls, prefix, err := d.Get_Chapter_Urls(string(body))
+	if err != nil {
+		logrus.WithField("downloader", "Download").Error("Error when get chapter urls")
+		return book, err
+	}
+	if !prefix {
+		re := regexp.MustCompile(`(https?)://([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})`)
+		match := re.FindStringSubmatch(url)
+		if match == nil {
+			logrus.WithField("downloader", "Download").Error("Error when get domain")
+			return book, errors.New("error when get domain")
+		}
+		for i := range chapter_urls {
+			chapter_urls[i] = match[0] + chapter_urls[i]
+		}
+	}
+	return book, nil
+}
 
+func (d *DownloaderImpl) Get_Book_Name(desc string) (string, error) {
+	match := d.name_regex.FindStringSubmatch(desc)
+	if len(match) < 1 || match[1] == "" {
+		logrus.WithField("downloader", "Get_Book_Name").Error("No book name found")
+		return "", errors.New("no book name found")
+	}
+	return match[1], nil
+}
+
+func (d *DownloaderImpl) Get_Chapter_Urls(content string) ([]string, bool, error) {
+	allmatch := d.chapter_regex.FindAllStringSubmatch(content, -1)
+	if len(allmatch) < 1 {
+		logrus.WithField("downloader", "Get_Chapter_Urls").Error("No chapter urls found")
+		return nil, false, errors.New("no chapter urls found")
+	}
+	var urls []string
+	for _, match := range allmatch {
+		urls = append(urls, match[1])
+	}
+	if strings.HasPrefix(urls[0], "http") {
+		return urls, true, nil
+	} else {
+		return urls, false, nil
+	}
 }
--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package godown
+
 import (
 	"fmt"
+	"runtime"
+	"path"
+	nested "github.com/antonfisher/nested-logrus-formatter"
+	"github.com/sirupsen/logrus"
 )
-func main() { 
+
+func main() {
+	logrus.SetFormatter(&nested.Formatter{
+		HideKeys:    true,
+		FieldsOrder: []string{"component"},
+		CallerFirst: true,
+		CustomCallerFormatter: func(f *runtime.Frame) string {
+			// 只显示文件名和行号
+			_, filename := path.Split(f.File)
+			return fmt.Sprintf(" %s:%d", filename, f.Line)
+		},
+	})
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetReportCaller(true)
+	logrus.WithField("component", "main").Info("Starting server...")
+
 	fmt.Print("输入小说主页url:")
 	var url string
 	fmt.Scanln(&url)
